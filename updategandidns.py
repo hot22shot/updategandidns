d298573d9fb774c1d03807134b03bc61217b6e84
hot22shot <33095930+hot22shot@users.noreply.github.com>
2018-12-11 10:58:25 +0100

    Update updategandidns.py
    log seulement en cas de changement d'adresse ou d'erreur lors de la recup de l'adresse courante.

--- updategandidns.py
+++ updategandidns.py
@@ -6,7 +6,14 @@ import json
 import logging
 import unicodedata
 
-logging.basicConfig(filename='/usr/local/scripts/updatedns/updatedns.log',level=logging.INFO)
+# Nom du fichier de log
+logfile = 'updateDNSGandi.log'
+
+# Répertoire du fichier de log
+logpath = '/usr/local/scripts/updatedns/'
+
+# Fichier de log
+logging.basicConfig(filename=logpath+logfile,level=logging.INFO)
 
 # URL de la page retournant l'ip publique au format ipv4
 urlpage = 'https://v4.ifconfig.co/json'
@@ -20,7 +27,7 @@ mydomain = 'A MODIFIER'
 # Horodatage
 asctime = time.asctime( time.localtime() )
 
-# headers & cie
+# headers gandi
 ghg = {
     'X-Api-Key': '{0}'.format(apikey)
     }
@@ -28,6 +35,8 @@ ghp = {
     'Content-Type': 'application/json',
     'X-Api-Key': '{0}'.format(apikey),
     }
+
+# user-agent
 ua = {
     'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:59.0) Gecko/20100101 Firefox/59.0'
     }
@@ -39,18 +48,17 @@ try:
     zoneuuid=requests.get("https://dns.api.gandi.net/api/v5/domains/{0}".format(mydomain),headers=ghg).json()['zone_uuid']
     # récupération de l'ip configurée chez gandi
     oldip = requests.get("https://dns.api.gandi.net/api/v5/zones/{0}/records/%40/A".format(zoneuuid),headers=ghg).json()['rrset_values'][0]
-    logging.info("{0} : l'adresse ip enregistrée chez gandi est {1}.".format(asctime,oldip))
     # récupération de l'adresse ip actuelle
     currip = requests.get("{0}".format(urlpage),headers=ua).json()['ip']
     pattern = re.compile("^\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}$")
     result = re.search(pattern,currip,0)
     if result == None:
         logging.info("{0} : pas d'adresse ip actuelle trouvée !!".format(asctime))
-        sys.exit()
-    else:
-        logging.info("{0} : l'adresse ip actuelle est {1}.".format(asctime,currip))
+        sys.exit()     
     # Comparaison et mise à jour si besoin
     if oldip != currip:
+        logging.info("{0} : l'adresse ip enregistrée chez gandi est {1}.".format(asctime,oldip))
+        logging.info("{0} : l'adresse ip actuelle est {1}.".format(asctime,currip))
         # On cree un snapshot de la zone pour restauration en cas de pépin
         snap = requests.post("https://dns.api.gandi.net/api/v5/zones/{0}/snapshots".format(zoneuuid),headers=ghp).json()['message']
         # Mise a jour de la zone
